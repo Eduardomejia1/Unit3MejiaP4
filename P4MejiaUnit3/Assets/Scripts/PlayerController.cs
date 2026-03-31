5ae3f170b6181e998ddab728b7d681af93f99f44
Eduardomejia1 <730845@student.dusd.net>
2026-03-31 11:48:38 -0700

    Added a backround script
    Added a backround script and attatched it to background

--- P4MejiaUnit3/Assets/Scripts/PlayerController.cs
+++ P4MejiaUnit3/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,27 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce = 10;
     public float gravityModifer;
+    public bool isOnGround = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifer;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
