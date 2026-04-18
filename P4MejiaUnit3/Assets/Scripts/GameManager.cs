e3c90cb80829d7ce21639e117020808b917f0b67
Eduardomejia1 <730845@student.dusd.net>
2026-04-18 12:48:21 -0700

    Fixed Game Manager Script
    Fixed errors on Game Manager script.

--- P4MejiaUnit3/Assets/Scripts/GameManager.cs
+++ P4MejiaUnit3/Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     private PlayerController playerControllerScript;
     public Transform startingPoint;
     public float lerpSpeed;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,15 +21,10 @@ public class GameManager : MonoBehaviour
         StartCoroutine(PlayIntro());
     }
 
-    private string PlayIntro()
-    {
-        throw new NotImplementedException();
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if(!playerControllerScript.gameOver)
+        if (!playerControllerScript.gameOver)
         {
             score += 2;
         }
@@ -38,31 +33,29 @@ public class GameManager : MonoBehaviour
             score++;
         }
         Debug.Log("Score: " + score);
-    IEnumerator PlayIntro()
-        {
-            Vector3 startPos = playerControllerScript.transform.position;
-            Vector3 endPos = startingPoint.position;
-            float journeyLength = Vector3.Distance(startPos, endPos);
-            float startTime = Time.time;
-
-            float distanceCovered = (Time.time - startTime) * lerpSpeed;
-            float fractionOfJourney = distanceCovered / journeyLength;
-
-            playerControllerScript.GetComponent<Animator>().SetFloat("Speed_Multiplier", 0.5f);
-
-            while (fractionOfJourney < 1)
-            {
-                distanceCovered = (Time.time - startTime) * lerpSpeed;
-                fractionOfJourney = distanceCovered / journeyLength;
-                playerControllerScript.transform.position = Vector3.Lerp(startPos, endPos, fractionOfJourney);
-                yield return null;
-            }
-
-            playerControllerScript.GetComponent<Animator>().SetFloat("Speed_Mulitplier", 1.0f);
-            playerControllerScript.gameOver = false;
-            }
-
-            
-
     }
+    IEnumerator PlayIntro()
+    {
+        Vector3 startPos = playerControllerScript.transform.position;
+        Vector3 endPos = startingPoint.position;
+        float journeyLength = Vector3.Distance(startPos, endPos);
+        float startTime = Time.time;
+
+        float distanceCovered = (Time.time - startTime) * lerpSpeed;
+        float fractionOfJourney = distanceCovered / journeyLength;
+
+        playerControllerScript.GetComponent<Animator>().SetFloat("Speed_Multiplier", 0.5f);
+
+        while (fractionOfJourney < 1)
+        {
+            distanceCovered = (Time.time - startTime) * lerpSpeed;
+            fractionOfJourney = distanceCovered / journeyLength;
+            playerControllerScript.transform.position = Vector3.Lerp(startPos, endPos, fractionOfJourney);
+            yield return null;
+        }
+
+        playerControllerScript.GetComponent<Animator>().SetFloat("Speed_Mulitplier", 1.0f);
+        playerControllerScript.gameOver = false;
+    }
+
 }
